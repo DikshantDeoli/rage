2b2b0083946e4c3c72204da9ce8d98e0276cb955
Dikshant Deoli <69298906+DikshantDeoli@users.noreply.github.com>
2023-09-19 04:08:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/api/receive-data', (req, res) => {
+    const receivedData = req.body;
+    console.log(receivedData)
+    res.json({ message: 'Data received successfully' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
