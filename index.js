5c81c1c1a3a537d52e850a947f7ab7c8ae0af04f
Dikshant Deoli <69298906+DikshantDeoli@users.noreply.github.com>
2023-09-19 16:33:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
